bf55874771d0fc3e22f9d9fcca019fbcd781122a
Olena Litovska <litovskaolena@gmail.com>
2023-03-29 00:30:07 +0300

    Completed lab #2 with Shannon-Fano algorithm for data compression

---
+++ src/main/java/TestCons.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TestCons {
+    public static void main(String[] args) {
+        System.out.println("Кирилиця виводиться іїєґ");
+        
+        System.out.println( (new Scanner(System.in)).nextLine());
+    }
+}
